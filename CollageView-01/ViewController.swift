de8bfad9c91716592e0155cc40ba80e7c937d9ab
yccheok <yancheng.cheok@gmail.com>
2021-03-15 10:08:10 +0800

    Initial creation

---
+++ CollageView-01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollageView-01
+//
+//  Created by Cheok Yan Cheng on 15/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
