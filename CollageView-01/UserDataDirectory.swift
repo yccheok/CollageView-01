af78836c16ba4cccf64e2925778226dbc8bb2517
yccheok <yancheng.cheok@gmail.com>
2021-03-19 00:49:31 +0800

    UserDataDirectory

--- CollageView-01/UserDataDirectory.swift
+++ CollageView-01/UserDataDirectory.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum UserDataDirectory: String {
+    case Extract = "extract"
+    case Camera = "camera"
+    case Mic = "mic"
+    case Attachment = "attachment"
+    case Recording = "recording"
+    case RestoreAttachment = "restore_attachment"
+    case RestoreRecording = "restore_recording"
+    case Backup = "backup"
+    
+    func get() -> URL {
+        var url = Utils.getUserDataDirectory()
+        url.appendPathComponent(self.rawValue)
+        return url
+    }
+}
