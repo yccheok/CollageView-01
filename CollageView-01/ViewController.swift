e638e2daef5e09e669f65244fbe1bd15c18c79e7
yccheok <yancheng.cheok@gmail.com>
2021-03-19 00:59:04 +0800

    WIP

--- CollageView-01/ViewController.swift
+++ CollageView-01/ViewController.swift
@@ -12,14 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet var collageView: CollageView!
     @IBOutlet var scrollableCollageView: ScrollableCollageView!
     var index: Int = 0
+    let names = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg"]
     
     @IBAction func set(_ sender: Any) {
         var attachments: [Attachment] = []
         
         for i in (0...4).reversed() {
-            let image = UIImage(named: String(i))!
+            let image = UIImage(named: Utils.getAttachmentPath(Directory.Default, names[index]).path)!
             
-            let attachment = Attachment(name: String(i), width: (Double)(image.size.width), height: (Double)(image.size.height))
+            let attachment = Attachment(
+                directory: Directory.Default,
+                name: names[i],
+                width: (Double)(image.size.width),
+                height: (Double)(image.size.height)
+            )
             
             attachments.append(attachment)
         }
@@ -29,14 +35,19 @@ class ViewController: UIViewController {
     }
     
     @IBAction func add(_ sender: Any) {
-        let image = UIImage(named: String(index))!
+        let image = UIImage(named: Utils.getAttachmentPath(Directory.Default, names[index]).path)!
         
-        let attachment = Attachment(name: String(index), width: (Double)(image.size.width), height: (Double)(image.size.height))
+        let attachment = Attachment(
+            directory: Directory.Default,
+            name: names[index],
+            width: (Double)(image.size.width),
+            height: (Double)(image.size.height)
+        )
         
         collageView.addAttachment(attachment)
         scrollableCollageView.addAttachment(attachment)
         
-        index = index + 1
+        index = (index + 1) % names.count
     }
     
     override func viewDidLoad() {
