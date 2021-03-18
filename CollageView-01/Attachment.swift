bdc36123f88f9ec855ff9f1a51bf25884531df70
yccheok <yancheng.cheok@gmail.com>
2021-03-19 00:48:53 +0800

    getPath

--- CollageView-01/Attachment.swift
+++ CollageView-01/Attachment.swift
@@ -8,9 +8,14 @@
 import Foundation
 
 struct Attachment : Hashable {
+    let directory: Directory
     let name: String
     let width: Double
     let height: Double
+    
+    func getPath() -> URL {
+        return Utils.getAttachmentPath(directory, name)
+    }
     
     func isSameSize(_ attachment: Attachment) -> Bool {
         return width == attachment.width && height == attachment.height
